540021b2a0c465881b8ac8b748acadf2803e5d62
Saulo Santos <saulo.macedoprojetos@gmail.com>
2020-04-09 11:09:27 -0300

    Atualiza query bd

--- bd/scipts/bd_dados_qsa_cnpj_DQL.sql
+++ bd/scipts/bd_dados_qsa_cnpj_DQL.sql
@@ -1,5 +1,7 @@
+USE bd_dados_qsa_cnpj;
+
 SELECT * FROM tab_cnae;
 
 SELECT * FROM tab_situacao_cadastral;
 
-SELECT * FROM cnpj_dados_cadastrais_pj;
+SELECT * FROM cnpj_dados_cadastrais_pj WHERE cnae_fiscal LIKE '47%' AND municipio LIKE '%santa%';
