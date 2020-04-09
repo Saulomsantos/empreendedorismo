e256a901f99e40bfea39714bebe46507f7ce19ac
Saulo Santos <saulo.macedoprojetos@gmail.com>
2020-04-09 11:09:59 -0300

    Adiciona required

--- backend/empreendedorismo.webapi/empreendedorismo.webapi/ViewModel/FiltroViewModel.cs
+++ backend/empreendedorismo.webapi/empreendedorismo.webapi/ViewModel/FiltroViewModel.cs
@@ -1,10 +1,13 @@
-﻿namespace empreendedorismo.webapi.ViewModel
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace empreendedorismo.webapi.ViewModel
 {
     /// <summary>
     /// Classe responsável pelo modelo de filtro
     /// </summary>
     public class FiltroViewModel
     {
+        [Required(ErrorMessage = "Informe o CNAE para pesquisa")]
         public int CnaeFiscal { get; set; }
         public string Bairro { get; set; }
         public string Municipio { get; set; }
