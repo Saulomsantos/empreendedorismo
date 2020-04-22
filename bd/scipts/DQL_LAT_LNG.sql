b2f8882e0f055706d1df841125eb60f29492cf3a
Saulo Santos <saulo.macedoprojetos@gmail.com>
2020-04-22 15:41:53 -0300

    Adiciona recursos

--- bd/scipts/DQL_LAT_LNG.sql
+++ bd/scipts/DQL_LAT_LNG.sql
@@ -1,5 +1,13 @@
-SELECT cod_empresa, CNPJ, lat, lng FROM cnpj_dados_cadastrais_pj WHERE cod_empresa > 1000 AND cod_empresa < 1701;
+SELECT cod_empresa, CNPJ, lat, lng FROM cnpj_dados_cadastrais_pj WHERE cod_empresa >= 5600 AND cod_empresa <= 10000;
 
-SELECT * FROM cnpj_dados_cadastrais_pj WHERE cod_empresa = 1065;
+/*
+SELECT cod_empresa, CNPJ, lat, lng FROM cnpj_dados_cadastrais_pj WHERE cod_empresa = 2934;
+
+SELECT * FROM cnpj_dados_cadastrais_pj WHERE cod_empresa = 10498;
+
+UPDATE cnpj_dados_cadastrais_pj SET lat = '-23.4987424' WHERE cod_empresa = 10498;
+UPDATE cnpj_dados_cadastrais_pj SET lng = '-46.576317' WHERE cod_empresa = 10498;
+
+SELECT cod_empresa, CNPJ, lat, lng FROM cnpj_dados_cadastrais_pj WHERE numero LIKE 'SN' ORDER BY (cod_empresa);
 
 SELECT COUNT (cnpj) FROM cnpj_dados_cadastrais_pj WHERE situacao_cadastral = 2;
