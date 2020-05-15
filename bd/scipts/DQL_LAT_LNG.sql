fe093816b113483399685b22947213df219781e8
Saulo Santos <saulo.macedoprojetos@gmail.com>
2020-05-15 08:19:37 -0300

    Adiciona query que atualiza lat lng

--- bd/scipts/DQL_LAT_LNG.sql
+++ bd/scipts/DQL_LAT_LNG.sql
@@ -1,13 +1,24 @@
-SELECT cod_empresa, CNPJ, lat, lng FROM cnpj_dados_cadastrais_pj WHERE cod_empresa >= 5600 AND cod_empresa <= 10000;
+SELECT cod_empresa, CNPJ, lat, lng FROM cnpj_dados_cadastrais_pj WHERE cod_empresa >= 650001 AND cod_empresa <= 700000 AND lat is null;
 
 /*
-SELECT cod_empresa, CNPJ, lat, lng FROM cnpj_dados_cadastrais_pj WHERE cod_empresa = 2934;
 
-SELECT * FROM cnpj_dados_cadastrais_pj WHERE cod_empresa = 10498;
+SELECT COUNT (cod_empresa) FROM cnpj_dados_cadastrais_pj WHERE cod_empresa >= 600001 AND cod_empresa <= 650000 AND lat is null;
 
-UPDATE cnpj_dados_cadastrais_pj SET lat = '-23.4987424' WHERE cod_empresa = 10498;
-UPDATE cnpj_dados_cadastrais_pj SET lng = '-46.576317' WHERE cod_empresa = 10498;
+SELECT cod_empresa, CNPJ, lat, lng FROM cnpj_dados_cadastrais_pj WHERE cod_empresa >= 600001 AND cod_empresa <= 650000 AND lat is null;
+
+
+SELECT * FROM cnpj_dados_cadastrais_pj WHERE cod_empresa = 679451;
+
+
+UPDATE cnpj_dados_cadastrais_pj SET lat = '-23.6089523' WHERE cod_empresa = 679451;
+UPDATE cnpj_dados_cadastrais_pj SET lng = '-46.4078592' WHERE cod_empresa = 679451;
+
+
 
 SELECT cod_empresa, CNPJ, lat, lng FROM cnpj_dados_cadastrais_pj WHERE numero LIKE 'SN' ORDER BY (cod_empresa);
 
 SELECT COUNT (cnpj) FROM cnpj_dados_cadastrais_pj WHERE situacao_cadastral = 2;
+
+-- Brasilândia, Barra Funda
+
+*/
